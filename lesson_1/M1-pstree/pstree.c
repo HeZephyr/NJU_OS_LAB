40e4806fd2c737d9348831cf35ee31fd468d0281
pursuit <2825841950@qq.com>
2024-03-01 10:58:22 +0800

    pstree开发：功能基本完成，存在一些TODO清单

--- lesson_1/M1-pstree/pstree.c
+++ lesson_1/M1-pstree/pstree.c
@@ -8,16 +8,30 @@
 #define true ((unsigned char) (1))
 #define false ((unsigned char) (0))
 
-unsigned char show_pids = false, numberic_sort = false, show_version = false;
+unsigned char normal = false, show_pids = false, numberic_sort = false, show_version = false;
 
 typedef struct pidinfo {
-	char name[50]; // 进程名称
-	pid_t pid; // 进程id
-	pid_t ppid; // 父进程id
-	struct pidinfo* next; // 指向下一个进程
+	char name[50];		// 进程名称
+	pid_t pid;		// 进程id
+	pid_t ppid; 		// 父进程id
+	struct pidinfo* next;	// 指向下一个进程
 }PidInfo;
 PidInfo pidinfos[1000];
 int pid_count;
+
+int childrens_index[500];
+
+// 定义进程树节点的结构体
+typedef struct processtree {
+	pid_t pid;	// 进程id
+	char name[50];	// 进程名称
+	struct childprocesses* children;
+}Processtree;
+// 定义子进程链表结点的结构体
+struct childprocesses {
+	struct childprocesses* next;	// 指向下一个子进程的指针
+	Processtree* process;		// 指向当前子进程的指针
+};
 
 /*
  * 初始化操作
@@ -34,8 +48,12 @@ void init() {
  *  return: 经过处理的参数值
  */
 int get_command_line_ops(int argc, char* argv[]) {
-	// 目前只处理一个参数值的情况
-	assert(argc == 2);
+	// 目前只处理一个和两个参数值的情况
+	assert(argc >= 1 && argc <= 2);
+	if (argc == 1) {
+		normal = true;
+		return 0;
+	}
 	if (!strcmp(argv[1], "-p") || !strcmp(argv[1], "--show-pids")) {
 		show_pids = true;
 	} else if (!strcmp(argv[1], "-n") || !strcmp(argv[1], "--numberic-sort")) {
@@ -112,6 +130,119 @@ void set_process_info() {
 	}
 }
 /*
+ * params: pid为父进程ID， index为要保存的子进程索引
+ * 查找指定父进程ID的所有紫禁城索引
+ */
+int find_all_childrens(pid_t pid, int index[]) {
+	int count = 0;
+	for (int i = 0; i < pid_count; i++) {
+		if (pidinfos[i].ppid == pid) {
+			index[count++] = i;
+		}
+	}	
+	return count;
+}
+void create_tree(Processtree* root, int tab_length) {
+	char str[100] = {0};
+	int childrens_index[500] = {0}; // 所有子进程在pidinfos中的下标
+	int count = find_all_childrens(root->pid, childrens_index);
+	
+	if (!count) { // 叶子结点，没有孩子
+		printf("%s(%d)", root->name, root->pid);	// TODO 还有结点，在/proc/pid/task/.../stat中
+		return;
+	}
+	sprintf(str, "%s(%d)-", root->name, root->pid);
+	printf("%s", str);
+
+	int flag = 0;
+	if (count > 1) {
+		printf("+-");
+		flag = 2;
+	}
+
+	root->children = (struct childprocesses*)malloc(sizeof(struct childprocesses));
+	if (root->children == NULL) {
+		fprintf(stderr, "分配内存失败\n");
+		exit(1);
+	}
+	struct childprocesses* node = root->children;
+
+	for (int i = 0; i < count; i++) {
+		node->process = (Processtree*)malloc(sizeof(Processtree));
+		if (!node->process) {
+			fprintf(stderr, "分配内存失败\n");
+			exit(1);
+		}
+		node->process->pid = pidinfos[childrens_index[i]].pid;
+		strcpy(node->process->name, pidinfos[childrens_index[i]].name);
+		create_tree(node->process, strlen(str) + tab_length + flag);
+		if (i + 1 < count) {
+			node->next = (struct childprocesses*)malloc(sizeof(struct childprocesses));
+			if (!node->next) {
+				fprintf(stderr, "分配内存失败\n");
+				exit(1);
+			}	
+			node = node->next;
+			printf("\n");
+			for (int j = 0; j < strlen(str) + tab_length; j++) {
+				printf(" ");
+			}
+			printf("|-");
+		}
+	}
+	free(root);
+}
+void create_tree_nopid(Processtree* root, int tab_length) {
+	char str[100] = {0};
+	int childrens_index[500] = {0}; // 所有子进程在pidinfos中的下标
+	int count = find_all_childrens(root->pid, childrens_index);
+	
+	if (!count) { // 叶子结点，没有孩子
+		printf("%s", root->name);	// TODO 还有结点，在/proc/pid/task/.../stat中
+		return;
+	}
+	sprintf(str, "%s-", root->name);
+	printf("%s", str);
+
+	int flag = 0;
+	if (count > 1) {
+		printf("+-");
+		flag = 2;
+	}
+
+	root->children = (struct childprocesses*)malloc(sizeof(struct childprocesses));
+	if (root->children == NULL) {
+		fprintf(stderr, "分配内存失败\n");
+		exit(1);
+	}
+	struct childprocesses* node = root->children;
+
+	for (int i = 0; i < count; i++) {
+		node->process = (Processtree*)malloc(sizeof(Processtree));
+		if (!node->process) {
+			fprintf(stderr, "分配内存失败\n");
+			exit(1);
+		}
+		node->process->pid = pidinfos[childrens_index[i]].pid;
+		strcpy(node->process->name, pidinfos[childrens_index[i]].name);
+		create_tree_nopid(node->process, strlen(str) + tab_length + flag);
+		if (i + 1 < count) {
+			node->next = (struct childprocesses*)malloc(sizeof(struct childprocesses));
+			if (!node->next) {
+				fprintf(stderr, "分配内存失败\n");
+				exit(1);
+			}	
+			node = node->next;
+			printf("\n");
+			for (int j = 0; j < strlen(str) + tab_length; j++) {
+				printf(" ");
+			}
+			printf("|-");
+		}
+	}
+	free(root);
+}
+/*
  * 返回版本信息
  */
 char* get_version() {
@@ -159,14 +290,26 @@ int main(int argc, char *argv[]) {
 		printf("Illegal parameter formats!\n");
 		return -1;
 	}
+	if (normal == true) {
+		set_process_info();
+		Processtree* root = (Processtree*)malloc(sizeof(Processtree));
+		root->pid = pidinfos[0].pid;
+		strcpy(root->name, pidinfos[0].name);
+		create_tree_nopid(root, 0);
+	}
 	if (show_pids == true) {
 		set_process_info();
-		for (int i = 0; i < pid_count; i++) {
-			printf("%s, %d, %d\n", pidinfos[i].name, pidinfos[i].pid, pidinfos[i].ppid);
-		}
+		Processtree* root = (Processtree*)malloc(sizeof(Processtree));
+		root->pid = pidinfos[0].pid;
+		strcpy(root->name, pidinfos[0].name);
+		create_tree(root, 0);
 	}
 	if (numberic_sort == true) {
-	
+		set_process_info();
+		Processtree* root = (Processtree*)malloc(sizeof(Processtree));
+		root->pid = pidinfos[0].pid;
+		strcpy(root->name, pidinfos[0].name);
+		create_tree_nopid(root, 0);
 	}
 	if (show_version == true) {
 		printf("%s", get_version());	
